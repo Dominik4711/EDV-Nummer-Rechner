5d3a50fff83a462c6e5792b885ab4b83b914676a
drouver-helmuth <51882074+drouver-helmuth@users.noreply.github.com>
2020-11-12 23:13:15 +0100

    overhauled prüfziffer calculator
    added compatibility for 4 character class number

--- index.php
+++ index.php
@@ -17,27 +17,38 @@
   <?php
   if(isset($_POST["absenden"])){
 
-    $nummerIn=$_POST["nummerInput"];
+    $nummerIn=$_POST["nummerInput"];#
 
-    if(strlen($nummerIn)>=7){
-      $uic = trim($nummerIn, "\x00..\x1F-"); // clean array of special characters
-
+    if(strlen($nummerIn)>=4){
+      $uic = str_replace(' ', '', $nummerIn);
+      $uic = str_replace('-', '', $uic);
       $uic = str_split($uic); // Split string in elements of array
       $uicl = count($uic)-1; // Get the lenght of Array
-
-      for ($i = 1;  $i <= $uicl; $i+1){  // Hier ist noch eine Menge Arbeit. Ich versuche den Algorythmmus expandebel zu machen, sodass auch der 1440 z.b. dazu kann. Die Prüfziffer ensteht immer nach dem System 1212121 usw., die letzte Ziffer kann dabei als fixer Startpunkt gesehen werden.
-        if($i % 2 == 0){
-          $prf[$i] = $uic[$i];
-        } else {
-          $prf[$i] = $uic[$i] * 2 ;
-        }
+      for($i=$uicl; $i >= 0; $i = $i - 2){
+        $prf[$i] = $uic[$i] * 2;
       }
-      print_r($prf);
-      print_r($uic);
+      for($i=$uicl-1; $i >= 0; $i = $i - 2){
+        $prf[$i] = $uic[$i];
+      }
+      $przf = 0;
+      for($i=$uicl; $i >= 0; $i = $i-1){
+        if($prf[$i] > 9){
+          $pz = str_split($prf[$i]);
+          $prf[$i] = $pz[0] + $pz[1];
+        }
+        $przf = $prf[$i] + $przf;
 
-      echo $uicl;
-
-    echo("Die Nummer lautet: <b>".$nummerA[0].$nummerA[1].$nummerA[2]." ".$nummerA[3].$nummerA[4].$nummerA[5]."-".$pruefziffer."</b>");
+      }
+      $przf = str_split($przf);
+      if($przf[1] != 0){
+      $pruefziffer = 10 - $przf[1];
+    } else {
+      $pruefziffer = 0;
+    }
+    if($uicl > 5){
+      echo("Die Nummer lautet: <b>".$uic[0].$uic[1].$uic[2].$uic[3]." ".$uic[4].$uic[5].$uic[6]."-".$pruefziffer."</b>");
+    } elseif($uicl <= 5)
+    echo("Die Nummer lautet: <b>".$uic[0].$uic[1].$uic[2]." ".$uic[3].$uic[4].$uic[5]."-".$pruefziffer."</b>");
     }else{
       echo ("Gib eine geeignete Nummer ein! <br>Beispiel: 078 468)");
     }
